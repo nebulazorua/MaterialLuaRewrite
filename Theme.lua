23dba14e4af7a0cdb7082c8610e4df2b1029c3ec
supersonicfan111 <rainbowsmokefox@gmail.com>
2020-08-28 23:40:24 +0800

    Update Theme.lua

--- Theme.lua
+++ Theme.lua
@@ -17,11 +17,31 @@ function theme:AddComponent(component,primary,secondary)
 	self.Components[component]={Primary=primary;Secondary=secondary;}
 end
 
+function theme:Set(a,b)
+	if(self.Components[a])then
+		self.Components[a]=b
+	else
+		self[a]=b;
+	end
+end
+
 function theme.new(primary,secondary,components)
 	if(typeof(primary)~='Color3')then primary = Color3.fromRGB(124,37,255); end
 	if(typeof(secondary)~='Color3')then secondary = Color3.fromRGB(255,255,255); end	
 	if(typeof(components)~='table')then components={};end
 	
+	if(not components.Minimise)then
+		components.Minimise={
+			Primary=Color3.fromRGB(255,106,0);
+			Secondary=Color3.fromRGB(147,59,0);
+		}
+	end
+	if(not components.Maximise)then
+		components.Maximise={
+			Primary=Color3.fromRGB(25,255,0);
+			Secondary=Color3.fromRGB(0,255,110);
+		}
+	end
 	return setmetatable({
 		Primary=primary;
 		Secondary=secondary;
